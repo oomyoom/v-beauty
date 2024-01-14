be511de270944527a0c19771c83b83053e9821a2
oomyoom <oomyoom@gmail.com>
2024-01-14 07:32:38 +0700

    update home ui

--- lib/home/view/home_page.dart
+++ lib/home/view/home_page.dart
@@ -14,50 +14,255 @@ class HomePage extends StatelessWidget {
         preferredSize: Size.fromHeight(appBarHeight),
         child: const CustomAppBar(),
       ),
+      backgroundColor: Colors.grey[200],
       body: CustomScrollView(
         slivers: [
           SliverPadding(
             padding: EdgeInsets.symmetric(
-                horizontal: MediaQuery.of(context).size.width * 0.02),
-            sliver: SliverGrid(
-              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: 2,
-                crossAxisSpacing: 2,
-                mainAxisSpacing: 1,
-              ),
-              delegate: SliverChildBuilderDelegate(
-                (BuildContext context, int index) {
-                  return GridTile(
-                    child: ListTile(
-                      title: Container(
-                        width: MediaQuery.of(context).size.width * 0.03,
-                        height: MediaQuery.of(context).size.height * 0.13,
-                        decoration: BoxDecoration(
-                            image: DecorationImage(
-                                fit: BoxFit.contain,
-                                image: NetworkImage(productData[index].image)),
-                            border: Border.all(color: Colors.black, width: 0.4),
-                            borderRadius: BorderRadius.circular(16)),
-                      ),
-                      subtitle: Padding(
-                        padding: EdgeInsets.symmetric(
-                            vertical: MediaQuery.of(context).size.width * 0.02),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            Text(productData[index].title),
-                            Text(productData[index].price.toStringAsFixed(0) +
-                                ' ฿')
-                          ],
-                        ),
-                      ),
+                horizontal: MediaQuery.of(context).size.width * 0.04),
+            sliver: SliverToBoxAdapter(
+                child: Container(
+              height: MediaQuery.of(context).size.height * 0.2,
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(
+                      MediaQuery.of(context).size.width * 0.02),
+                  image: DecorationImage(
+                      fit: BoxFit.cover,
+                      image: NetworkImage(
+                          'https://images.jdmagicbox.com/rep/b2b/beauty-cosmetics/beauty-cosmetics-3.jpg'))),
+            )),
+          ),
+          SliverToBoxAdapter(
+            child: SizedBox(height: MediaQuery.of(context).size.height * 0.02),
+          ),
+          SliverPadding(
+            padding: EdgeInsets.symmetric(
+              horizontal: MediaQuery.of(context).size.width * 0.04,
+            ),
+            sliver: SliverToBoxAdapter(
+              child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      'ลิปสติก',
+                      style: Theme.of(context)
+                          .textTheme
+                          .titleLarge!
+                          .copyWith(fontWeight: FontWeight.bold),
                     ),
-                  );
-                },
-                childCount: productData.length,
-              ),
+                    TextButton(
+                      onPressed: () {},
+                      child: Text('ดูทั้งหมด'),
+                      style: TextButton.styleFrom(foregroundColor: Colors.pink),
+                    )
+                  ]),
             ),
           ),
+          SliverToBoxAdapter(
+            child: SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+              child: Row(
+                  children: List.generate(
+                      productData.length,
+                      (index) => Padding(
+                          padding: EdgeInsets.symmetric(
+                              horizontal:
+                                  MediaQuery.of(context).size.width * 0.01),
+                          child: InkWell(
+                            child: Column(
+                              children: [
+                                Container(
+                                  width:
+                                      MediaQuery.of(context).size.width * 0.4,
+                                  height:
+                                      MediaQuery.of(context).size.height * 0.15,
+                                  decoration: BoxDecoration(
+                                      image: DecorationImage(
+                                          fit: BoxFit.fill,
+                                          image: NetworkImage(
+                                              productData[index].image))),
+                                ),
+                                Container(
+                                  color: Colors.white,
+                                  padding: EdgeInsets.all(
+                                      MediaQuery.of(context).size.width * 0.02),
+                                  width:
+                                      MediaQuery.of(context).size.width * 0.4,
+                                  height:
+                                      MediaQuery.of(context).size.height * 0.07,
+                                  child: Column(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    children: [
+                                      Text(
+                                        productData[index].title,
+                                        style: Theme.of(context)
+                                            .textTheme
+                                            .bodyMedium!,
+                                        overflow: TextOverflow.ellipsis,
+                                        maxLines: 1,
+                                      ),
+                                      SizedBox(
+                                          height: MediaQuery.of(context)
+                                                  .size
+                                                  .height *
+                                              0.003),
+                                      Row(
+                                        mainAxisAlignment:
+                                            MainAxisAlignment.spaceBetween,
+                                        children: [
+                                          Text(
+                                            '฿ ' +
+                                                productData[index]
+                                                    .price
+                                                    .toStringAsFixed(0),
+                                            style: Theme.of(context)
+                                                .textTheme
+                                                .bodyMedium!
+                                                .copyWith(color: Colors.pink),
+                                          ),
+                                          Row(
+                                            children: [
+                                              Icon(
+                                                Icons.star_rate_rounded,
+                                                color: Colors.yellow,
+                                                size: 20,
+                                              ),
+                                              Text(
+                                                '4.8',
+                                                style: Theme.of(context)
+                                                    .textTheme
+                                                    .bodyMedium!
+                                                    .copyWith(
+                                                        color:
+                                                            Color(0xFF615C62)),
+                                              ),
+                                            ],
+                                          ),
+                                        ],
+                                      )
+                                    ],
+                                  ),
+                                )
+                              ],
+                            ),
+                          )))),
+            ),
+          ),
+          SliverPadding(
+            padding: EdgeInsets.symmetric(
+              horizontal: MediaQuery.of(context).size.width * 0.04,
+            ),
+            sliver: SliverToBoxAdapter(
+              child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      'บลัชออน',
+                      style: Theme.of(context)
+                          .textTheme
+                          .titleLarge!
+                          .copyWith(fontWeight: FontWeight.bold),
+                    ),
+                    TextButton(
+                      onPressed: () {},
+                      child: Text('ดูทั้งหมด'),
+                      style: TextButton.styleFrom(foregroundColor: Colors.pink),
+                    )
+                  ]),
+            ),
+          ),
+          SliverToBoxAdapter(
+            child: SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+              child: Row(
+                  children: List.generate(
+                      productData2.length,
+                      (index) => Padding(
+                          padding: EdgeInsets.symmetric(
+                              horizontal:
+                                  MediaQuery.of(context).size.width * 0.01),
+                          child: InkWell(
+                            child: Column(
+                              children: [
+                                Container(
+                                  width:
+                                      MediaQuery.of(context).size.width * 0.4,
+                                  height:
+                                      MediaQuery.of(context).size.height * 0.15,
+                                  decoration: BoxDecoration(
+                                      image: DecorationImage(
+                                          fit: BoxFit.fill,
+                                          image: NetworkImage(
+                                              productData2[index].image))),
+                                ),
+                                Container(
+                                  color: Colors.white,
+                                  padding: EdgeInsets.all(
+                                      MediaQuery.of(context).size.width * 0.02),
+                                  width:
+                                      MediaQuery.of(context).size.width * 0.4,
+                                  height:
+                                      MediaQuery.of(context).size.height * 0.07,
+                                  child: Column(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    children: [
+                                      Text(
+                                        productData[index].title,
+                                        style: Theme.of(context)
+                                            .textTheme
+                                            .bodyMedium!,
+                                        overflow: TextOverflow.ellipsis,
+                                        maxLines: 1,
+                                      ),
+                                      SizedBox(
+                                          height: MediaQuery.of(context)
+                                                  .size
+                                                  .height *
+                                              0.003),
+                                      Row(
+                                        mainAxisAlignment:
+                                            MainAxisAlignment.spaceBetween,
+                                        children: [
+                                          Text(
+                                            '฿ ' +
+                                                productData2[index]
+                                                    .price
+                                                    .toStringAsFixed(0),
+                                            style: Theme.of(context)
+                                                .textTheme
+                                                .bodyMedium!
+                                                .copyWith(color: Colors.pink),
+                                          ),
+                                          Row(
+                                            children: [
+                                              Icon(
+                                                Icons.star_rate_rounded,
+                                                color: Colors.yellow,
+                                                size: 20,
+                                              ),
+                                              Text(
+                                                '4.9',
+                                                style: Theme.of(context)
+                                                    .textTheme
+                                                    .bodyMedium!
+                                                    .copyWith(
+                                                        color:
+                                                            Color(0xFF615C62)),
+                                              ),
+                                            ],
+                                          ),
+                                        ],
+                                      )
+                                    ],
+                                  ),
+                                )
+                              ],
+                            ),
+                          )))),
+            ),
+          )
         ],
       ),
     );
--- lib/models/product.dart
+++ lib/models/product.dart
@@ -14,24 +14,47 @@ List productData = [
       title: 'ลิปสติกสีแดง',
       description: 'ลิป',
       image:
-          'https://img.wongnai.com/p/1920x0/2023/05/08/051680b1d23a4709919b3dfbfdce7e0c.jpg',
+          'https://images.pexels.com/photos/1625037/pexels-photo-1625037.jpeg',
       price: 50),
   Product(
       title: 'ลิปสติกสีชมพู',
       description: 'ลิป',
       image:
-          'https://img.wongnai.com/p/1920x0/2023/05/08/051680b1d23a4709919b3dfbfdce7e0c.jpg',
+          'https://images.pexels.com/photos/1625037/pexels-photo-1625037.jpeg',
       price: 60),
   Product(
       title: 'ลิปสติกสีส้ม',
       description: 'ลิป',
       image:
-          'https://img.wongnai.com/p/1920x0/2023/05/08/051680b1d23a4709919b3dfbfdce7e0c.jpg',
+          'https://images.pexels.com/photos/1625037/pexels-photo-1625037.jpeg',
       price: 70),
   Product(
-      title: 'ลิปสติกสีเหลือง',
+      title: 'ลิปสติกสีเหลืองงงงงงงงงงงงงง',
       description: 'ลิป',
       image:
-          'https://img.wongnai.com/p/1920x0/2023/05/08/051680b1d23a4709919b3dfbfdce7e0c.jpg',
+          'https://images.pexels.com/photos/1625037/pexels-photo-1625037.jpeg',
+      price: 80)
+];
+
+List productData2 = [
+  Product(
+      title: 'บลัชออนสีแดง',
+      description: 'ลิป',
+      image: 'https://live.staticflickr.com/5297/5457270373_97c0b85da3_b.jpg',
+      price: 50),
+  Product(
+      title: 'บลัชออนสีชมพู',
+      description: 'ลิป',
+      image: 'https://live.staticflickr.com/5297/5457270373_97c0b85da3_b.jpg',
+      price: 60),
+  Product(
+      title: 'บลัชออนสีส้ม',
+      description: 'ลิป',
+      image: 'https://live.staticflickr.com/5297/5457270373_97c0b85da3_b.jpg',
+      price: 70),
+  Product(
+      title: 'บลัชออนสีเหลืองงงงงงงงงงงงงง',
+      description: 'ลิป',
+      image: 'https://live.staticflickr.com/5297/5457270373_97c0b85da3_b.jpg',
       price: 80)
 ];
